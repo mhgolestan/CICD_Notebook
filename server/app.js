0b183e9693c57f72e375b541df97f2e6891a6d11
mhgolestan <golestan1369@gmail.com>
2025-08-03 16:52:22 +0200

    Streamline GitHub Actions pipeline and update configuration
    - Replaced `npm install:all` with `npm run install:all` for accurate script execution.
    - Renamed `lint` command to `eslint` in workflow for better clarity.
    - Updated `package.json` to reflect the renamed `eslint` scripts and removed redundant commands.
    - Replaced static asset directory in `server/app.js` with dynamic path resolution using `path.join`.
    - Removed Discord notification steps from the workflow to simplify CI/CD pipeline.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const path = require('path')
 require('express-async-errors')
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
@@ -24,7 +25,8 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, '../client/dist')))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
