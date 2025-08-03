533a093c87277d30e6580b76a51e2d663f39ccb0
mhgolestan <golestan1369@gmail.com>
2025-08-03 21:57:27 +0200

    Add website health check workflow and new API endpoints
    - Created `.github/workflows/health-check.yml` to monitor website health.
    - Added `/health` and `/version` endpoints to the server for monitoring and version tracking.
    - Included Discord notifications for health check success and failure.

--- server/index.js
+++ server/index.js
@@ -5,3 +5,11 @@ const logger = require('./utils/logger')
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
